6e803ebf4f8ef103b2d5f0954588f705d681e71b
Isaac <150881372+AiFSD@users.noreply.github.com>
2025-03-09 12:06:52 -0400

    Update Heading.jsx

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMicrosoft } from "@fortawesome/free-brands-svg-icons"; 
-import "../styles/Heading.css";
+import "../styles/heading.css";
 
 const Heading = () => {
   return (
